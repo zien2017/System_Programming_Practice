2200f5c677a76b2069abc32cd9bf0b868d4190be
branwn <ziyuan1999@outlook.com>
2022-03-18 16:14:05 +0800

    sample server client

--- TCP-Socket Hot Potato/cmake-build-debug/CMakeFiles/Makefile.cmake
+++ TCP-Socket Hot Potato/cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -41,6 +41,8 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
+  "CMakeFiles/client.dir/DependInfo.cmake"
+  "CMakeFiles/server.dir/DependInfo.cmake"
   "CMakeFiles/ringmaster.dir/DependInfo.cmake"
   "CMakeFiles/player.dir/DependInfo.cmake"
   )
