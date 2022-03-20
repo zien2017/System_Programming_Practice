bb7f3a94521f303ec83c85b7af4dd5802b33a5cd
branwn <ziyuan1999@outlook.com>
2022-03-20 14:12:56 +0800

    refactor select server

--- TCP-Socket Hot Potato/socket_select_server.c
+++ TCP-Socket Hot Potato/socket_select_server.c
@@ -24,46 +24,33 @@ void *get_in_addr(struct sockaddr *sa)
 	return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-int main(void)
-{
-    fd_set master;    // master file descriptor list
-    fd_set read_fds;  // temp file descriptor list for select()
-    int fdmax;        // maximum file descriptor number
+int server_setup() {
 
     int listener;     // listening socket descriptor
-    int newfd;        // newly accept()ed socket descriptor
-    struct sockaddr_storage remoteaddr; // client address
-    socklen_t addrlen;
 
-    char buf[256];    // buffer for client data
-    int nbytes;
-
-	char remoteIP[INET6_ADDRSTRLEN];
-
-    int yes=1;        // for setsockopt() SO_REUSEADDR, below
-    int i, j, rv;
+    int yes = 1;        // for setsockopt() SO_REUSEADDR, below
+    int rv;
 
 	struct addrinfo hints, *ai, *p;
 
-    FD_ZERO(&master);    // clear the master and temp sets
-    FD_ZERO(&read_fds);
 
 	// get us a socket and bind it
 	memset(&hints, 0, sizeof hints);
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
+
 	if ((rv = getaddrinfo(NULL, PORT, &hints, &ai)) != 0) {
-		fprintf(stderr, "selectserver: %s\n", gai_strerror(rv));
+		fprintf(stderr, "select_server: %s\n", gai_strerror(rv));
 		exit(1);
 	}
-	
+
 	for(p = ai; p != NULL; p = p->ai_next) {
     	listener = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
-		if (listener < 0) { 
+		if (listener < 0) {
 			continue;
 		}
-		
+
 		// lose the pesky "address already in use" error message
 		setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
 
@@ -83,11 +70,39 @@ int main(void)
 
 	freeaddrinfo(ai); // all done with this
 
+
+
     // listen
     if (listen(listener, 10) == -1) {
         perror("listen");
         exit(3);
     }
+
+    return listener;
+
+}
+
+
+int main_loop (int listener) {
+    /////////////////////////////////////////////////////////////
+    int newfd;        // newly accept()ed socket descriptor
+    struct sockaddr_storage remoteaddr; // client address
+
+
+    char buf[256];    // buffer for client data
+    int nbytes;
+
+    char remoteIP[INET6_ADDRSTRLEN];
+
+
+    fd_set master;    // master file descriptor list
+    fd_set read_fds;  // temp file descriptor list for select()
+    int fdmax;        // maximum file descriptor number
+    socklen_t addrlen;
+
+    int i, j;
+    FD_ZERO(&master);    // clear the master and temp sets
+    FD_ZERO(&read_fds);
 
     // add the listener to the master set
     FD_SET(listener, &master);
@@ -95,10 +110,12 @@ int main(void)
     // keep track of the biggest file descriptor
     fdmax = listener; // so far, it's this one
 
+
+
     // main loop
     for(;;) {
         read_fds = master; // copy it
-        if (select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1) {
+        if (select(fdmax + 1, &read_fds, NULL, NULL, NULL) == -1) {
             perror("select");
             exit(4);
         }
@@ -109,11 +126,11 @@ int main(void)
                 if (i == listener) {
                     // handle new connections
                     addrlen = sizeof remoteaddr;
-					newfd = accept(listener,
-						(struct sockaddr *)&remoteaddr,
-						&addrlen);
+                    newfd = accept(listener,
+                                   (struct sockaddr *)&remoteaddr,
+                                   &addrlen);
 
-					if (newfd == -1) {
+                    if (newfd == -1) {
                         perror("accept");
                     } else {
                         FD_SET(newfd, &master); // add to master set
@@ -121,11 +138,11 @@ int main(void)
                             fdmax = newfd;
                         }
                         printf("selectserver: new connection from %s on "
-                            "socket %d\n",
-							inet_ntop(remoteaddr.ss_family,
-								get_in_addr((struct sockaddr*)&remoteaddr),
-								remoteIP, INET6_ADDRSTRLEN),
-							newfd);
+                               "socket %d\n",
+                               inet_ntop(remoteaddr.ss_family,
+                                         get_in_addr((struct sockaddr*)&remoteaddr),
+                                         remoteIP, INET6_ADDRSTRLEN),
+                               newfd);
                     }
                 } else {
                     // handle data from a client
@@ -157,7 +174,11 @@ int main(void)
             } // END got new incoming connection
         } // END looping through file descriptors
     } // END for(;;)--and you thought it would never end!
-    
+
     return 0;
 }
 
+int main(void) {
+    int listener_fd = server_setup ();
+    main_loop(listener_fd);
+}
