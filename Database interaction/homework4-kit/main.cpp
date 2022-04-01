4b3d8c9984f7e46476464ba9b044f0f8463bc64d
bran <ziyuan1999@outlook.com>
2022-04-01 12:32:21 +0000

    create and drop tables

--- Database interaction/homework4-kit/main.cpp
+++ Database interaction/homework4-kit/main.cpp
@@ -1,44 +1,90 @@
 #include <iostream>
 #include <pqxx/pqxx>
+#include <string>
 
 #include "exerciser.h"
 
 using namespace std;
 using namespace pqxx;
 
-int main (int argc, char *argv[]) 
-{
+int tableCreator(connection * C) {
 
-  //Allocate & initialize a Postgres connection object
-  connection *C;
+	/* Create SQL statement */
 
-  try{
-    //Establish a connection to the database
-    //Parameters: database name, user name, user password
-    C = new connection("dbname=ACC_BBALL user=postgres password=passw0rd");
-    if (C->is_open()) {
-      cout << "Opened database successfully: " << C->dbname() << endl;
-    } else {
-      cout << "Can't open database" << endl;
-      return 1;
-    }
-  } catch (const std::exception &e){
-    cerr << e.what() << std::endl;
-    return 1;
-  }
+	std::string sql;
 
+	sql = "CREATE TABLE COMPANY("
+		  "ID INT PRIMARY KEY     NOT NULL,"
+		  "NAME           TEXT    NOT NULL,"
+		  "AGE            INT     NOT NULL,"
+		  "ADDRESS        CHAR(50),"
+		  "SALARY         REAL );";
 
-  //TODO: create PLAYER, TEAM, STATE, and COLOR tables in the ACC_BBALL database
-  //      load each table with rows from the provided source txt files
+	/* Create a transactional object. */
+	work W(*C);
 
+	/* Execute SQL query */
+	W.exec(sql);
+	W.commit();
+	cout << "Table created successfully" << endl;
+}
 
-  exercise(C);
+int tableCleaner(connection * C) {
 
+	/* Create SQL statement */
 
-  //Close database connection
-  C->disconnect();
+	std::string sql;
 
-  return 0;
+	sql = "DROP TABLE COMPANY"
+		  "  CASCADE ";
+
+	/* Create a transactional object. */
+	work W(*C);
+
+	/* Execute SQL query */
+	W.exec(sql);
+	W.commit();
+	cout << "Table dropped successfully" << endl;
 }
 
 
+int main(int argc, char *argv[])
+{
+
+	// Allocate & initialize a Postgres connection object
+	connection *C;
+
+	try
+	{
+		// Establish a connection to the database
+		// Parameters: database name, user name, user password
+		C = new connection("dbname=ACC_BBALL user=postgres password=passw0rd");
+		if (C->is_open())
+		{
+			cout << "Opened database successfully: " << C->dbname() << endl;
+		}
+		else
+		{
+			cout << "Can't open database" << endl;
+			return 1;
+		}
+	}
+	catch (const std::exception &e)
+	{
+		cerr << e.what() << std::endl;
+		return 1;
+	}
+
+
+	tableCleaner(C);
+	// TODO: create PLAYER, TEAM, STATE, and COLOR tables in the ACC_BBALL database
+	//       load each table with rows from the provided source txt files
+	tableCreator(C);
+
+	exercise(C);
+
+	// Close database connection
+	C->disconnect();
+
+	return 0;
+}
